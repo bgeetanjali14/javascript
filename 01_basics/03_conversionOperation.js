ebde2ecb314abf9ea419c8674e0a2c7d9685efe4
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2026-01-07 16:58:03 +0530

    Added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let age = "14"
+
+console.log(typeof age);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33 ,gets converted
+//"33abc" = > NaN
+// true => 1
+
+//********************OPERATIONS*********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2/3);
+
+let str1 = "hello"
+let str2 = " geet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); //some rule of js
+
+//if first is string then next all are considered as string or else nooooo
+
+console.log(+true);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
