c614027e2d5a315006cb13b8829b66214bee352a
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2026-01-08 14:12:49 +0530

    PUSHED

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "geet"
+const repoCount = 50
+
+// console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //string interpolation-makes placeholders
+
+
+const gameName = new String('geethc')
---
+++ 01_basics/memory.js
@@ -0,0 +1,29 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack - for Primitive datatypes,gives a copy of data
+//Heap - for Non primitive datatypes,gives reference of data
+
+let myYoutubename = "abcxyzdotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);   //abcxyzdotcom
+console.log(anothername);     //chaiaurcode
+
+let userOne = {
+    email = user@gmail.com
+    upi = "user@gpay.com"
+}
+
+
+let userTwo = userOne
+
+userTwo.email = "geet@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
