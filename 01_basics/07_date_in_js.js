51b06e4e8dc51d0a253f02a72b8c4f10232f29e0
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2026-01-08 16:19:41 +0530

    PUSHED

---
+++ 01_basics/07_date_in_js.js
@@ -0,0 +1,16 @@
+//DAtes
+
+let myDate = new Date()
+console.log(myDate.toString());
+
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toDateString());
+
+
+
+
+
+
+
