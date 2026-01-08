dfc4e1cf1d04babd762fb791d344703917309e65
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2026-01-08 14:43:16 +0530

    PUSHED

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+//or
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumber = 23.34567
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+
+
+
+// ++++++++++++++++++++++++++++ MATHSSSSSSSSSSS +++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.56));
+
+console.log(Math.floor(7890.543));
+
+console.log(Math.ceil(564.768));
+
+
+
+
+
+
+
