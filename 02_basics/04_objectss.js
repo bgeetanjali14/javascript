ca8fb7b8d73051a4bd772dd4ef1cf53f3acc6da1
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2026-01-09 22:03:25 +0530

    pUSHED

---
+++ 02_basics/04_objectss.js
@@ -0,0 +1,36 @@
+//object using constructors/singleton
+
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = true
+// console.log(tinderUser);
+
+/// Nested Objects
+const regularUser = {
+    email: "sam@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Geetanjali",
+            lastname: "Bejjalwar"
+        }
+    }
+}
+
+
+// console.log(regularUser.fullname.userfullname.lastname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3:"a", 4:"b"}
+
+// const obj3 = Object.assign({}, obj1, obj2)     //object.assign merges two or more objects+++
+// console.log(obj3);
+
+
+//or use spread method
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+
+
