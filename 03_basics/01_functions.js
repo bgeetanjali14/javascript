979ded35cd335f733ef69935cc601526596cdaed
Geetanjali Bejjalwar <bejjalwargeetanjali@gmail.com>
2026-01-10 22:40:38 +0530

    PUSHED

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+function saymyName(){
+    console.log("G");
+    console.log("E");
+    console.log("E");
+    console.log("T");
+}
+
+// saymyName()
+
+//function to add numbers
+
+function addTwoNumbers(Number1, Number2){
+    console.log(Number1 + Number2);
+}
+
+// addTwoNumbers(3, 4)
+
+// addTwoNumbers(3, "4")
+
+// addTwoNumbers(3, "a")
+
+addTwoNumbers(3, null)
